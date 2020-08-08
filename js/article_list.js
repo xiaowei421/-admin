3092a24b74f4615f21a815d9c3ed7376d30fcfba
zhengjiawei <2267746827@qq.com>
2020-08-08 11:47:27 +0800

    04-上传下拉选择筛选js

--- js/article_list.js
+++ js/article_list.js
@@ -1,21 +1,43 @@
 $(function() {
+
+
+
     //1,请求地址：/admin/category/list
     // 请求方式：get
     // 请求参数：无
     // 返回数据：文章
-    function shuoyou() {
-        init(BigNew.article_query, { perpage: 10 })
-    };
-    shuoyou()
-        // 封装查询
-    function init(url, data) {
+    init();
+
+
+    //2请求地址：/admin/category/list
+    // 请求方式：get
+    // 请求参数：无
+    // 返回数据：文章
+    注意点使用下拉列表需要自定义
+    $('#btnSearch').on('click', function() {
+        init();
         $.ajax({
-            url: url,
-            data: data,
+            url: BigNew.category_list,
             datatype: 'json',
             success: function(res) {
                 if (res.code === 200) {
-                    $('tbody').html(template('articletemplate', res.data))
+                    $('#selCategory').html(template('articleidtemplate', res));
+                };
+            }
+        });
+        return false
+    });
+
+
+    // 封装查询
+    function init() {
+        $.ajax({
+            url: BigNew.article_query,
+            data: { perpage: 10, page: 1, type: $('#selCategory').val(), state: $('#selStatus').val() },
+            datatype: 'json',
+            success: function(res) {
+                if (res.code === 200) {
+                    $('tbody').html(template('articletemplate', res.data));
                 };
             }
         });
